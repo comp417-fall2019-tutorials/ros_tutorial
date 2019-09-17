ecd6d37fcb979b718530145e60b5b0141c65c8d1
Stefan <stefanwapnick@outlook.com>
2019-09-17 14:09:12 -0400

    Added pub sub combination

--- scripts/publisher_node.py
+++ scripts/publisher_node.py
@@ -8,13 +8,14 @@ def send_vel_command(throttle, steering):
     motor_command = Twist()
     motor_command.linear.x = throttle
     motor_command.angular.z = steering
+
     motor_command_pub.publish(motor_command)
 
 
 if __name__ == '__main__':
 
     rospy.init_node('publisher_node', anonymous=True)
-    motor_command_pub = rospy.Publisher('/cmd_vel', Twist)
+    motor_command_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
--- scripts/subscriber_node.py
+++ scripts/subscriber_node.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python2
+
 import rospy
 from geometry_msgs.msg import Twist
 
 
-def on_cmd_callback(vel):
-    rospy.loginfo("throttle: %.2f, steering: %.2f" % (vel.linear_acceleration.x, vel.angular_velocity.z))
+def on_cmd_callback(motor_command):
+    throttle = motor_command.linear.x
+    steering = motor_command.angular.z
+    rospy.loginfo("throttle: %.2f, steering: %.2f" % (throttle, steering))
 
 
 if __name__ == '__main__':
@@ -11,6 +15,7 @@ if __name__ == '__main__':
     rospy.init_node('subscriber_node', anonymous=True)
     r = rospy.Rate(30)
 
+    rospy.Subscriber('/cmd_vel', Twist, on_cmd_callback)
     while not rospy.is_shutdown():
         r.sleep()
         rospy.spin()
